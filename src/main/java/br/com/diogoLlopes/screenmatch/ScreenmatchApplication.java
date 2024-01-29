77afdc7d8299f8884ea9039545e32282a944a24f
diogo_lopes <diogocrlopes@gmail.com>
2024-01-28 23:56:47 -0300

    services incluidos and model

--- src/main/java/br/com/diogoLlopes/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/diogoLlopes/screenmatch/ScreenmatchApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.diogoLlopes.screenmatch.model.DadosSerie;
 import br.com.diogoLlopes.screenmatch.service.ConsumoApi;
+import br.com.diogoLlopes.screenmatch.service.ConverteDados;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner{
@@ -17,8 +19,11 @@ public class ScreenmatchApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		ConsumoApi consumoApi = new ConsumoApi();
 		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
-		System.out.println(json);
 		
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
 	}
 
 }
